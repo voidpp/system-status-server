e8b20669bb61396b6dcc5dc075873dc9d82a7abf
Lajos Santa <santa.lajos@coldline.hu>
2014-11-15 18:33:08 +0100

    Remove some shit

--- loadprovider.py
+++ loadprovider.py
@@ -1,30 +1,12 @@
 
-import re
-import subprocess
+import os
 
 from dataproviderbase import DataProviderBase
 
 class LoadProvider(DataProviderBase):
-    def __init__(self):
-        self.pattern = re.compile('load average: ([0-9\.]{2,}), ([0-9\.]{2,}), ([0-9\.]{2,})')
 
     def fetch(self):
-        try:
-            output = subprocess.Popen('uptime', stdout=subprocess.PIPE).communicate()[0].strip()
-        except Exception as e:
-            print e
-            return dict()
-
-        matches = self.pattern.search(output)
-
-        if matches is None:
-            print "Regex search failed. Output: '%s'" % output
-            return dict()
 
         return dict(
-            load = [
-                float(matches.group(1)),
-                float(matches.group(2)),
-                float(matches.group(3))
-            ]
+            load = os.getloadavg()
         )
--- uptimeprovider.py
+++ uptimeprovider.py
@@ -1,24 +1,13 @@
 
-import re
+import psutil
+import time
 
 from dataproviderbase import DataProviderBase
 
 class UptimeProvider(DataProviderBase):
-    def __init__(self):
-        self.pattern = re.compile('([0-9\.]{2,}) ([0-9\.]{2,})')
 
     def fetch(self):
 
-        output = ''
-        with open('/proc/uptime') as file:
-            output = file.read()
-
-        matches = self.pattern.search(output)
-
-        if matches is None:
-            print "Regex search failed. Output: '%s'" % output
-            return dict()
-
         return dict(
-            uptime = float(matches.group(1))
+            uptime = int(time.time() - psutil.boot_time())
         )
